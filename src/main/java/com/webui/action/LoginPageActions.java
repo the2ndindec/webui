10b33e23e9ce2512f65fe182d96981147eb585dc
lijinglin <the2ndindec@163.com>
2018-11-27 14:06:56 +0800

    登陆方法添加step注释

--- src/main/java/com/webui/action/LoginPageActions.java
+++ src/main/java/com/webui/action/LoginPageActions.java
@@ -5,12 +5,14 @@ import java.io.IOException;
 import com.webui.pageObject.LoginPage;
 import com.webui.utils.ElementAction;
 import com.webui.utils.TestBaseCase;
+import io.qameta.allure.Step;
 
 public class LoginPageActions extends TestBaseCase {
 
 	ElementAction ea = new ElementAction();
 	LoginPage lp = new LoginPage();
 
+	@Step(value = "输入用户名/密码")
 	public void signInInit(String username, String password) throws IOException {
 		ea.type(lp.userName_Text(), username);
 		ea.type(lp.password_Text(), password);

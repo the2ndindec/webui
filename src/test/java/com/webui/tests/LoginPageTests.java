a70196ffa56e6dfc98e2777ef2228c768f1ca908
lijinglin <the2ndindec@163.com>
2018-11-28 11:39:00 +0800

    添加登陆模块测试方法

---
+++ src/test/java/com/webui/tests/LoginPageTests.java
@@ -0,0 +1,27 @@
+package com.webui.tests;
+
+import com.webui.action.LoginPageActions;
+import com.webui.pageObject.DefultPage;
+import com.webui.utils.Assertion;
+import com.webui.utils.ElementAction;
+import com.webui.utils.TestBaseCase;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+/**
+ * @author the2n
+ * @description 登陆功能模块测试
+ * @Data 2018/11/28 11:30
+ */
+public class LoginPageTests extends TestBaseCase {
+    LoginPageActions loginPageActions = new LoginPageActions();
+    ElementAction elementAction = new ElementAction();
+    DefultPage defultPage = new DefultPage();
+
+    @Test(description = "login")
+    public void TC_login() throws IOException {
+      loginPageActions.signInInit("admin", "123456");
+        Assertion.VerityString(elementAction.getText(defultPage.userName_1()),"admin");
+    }
+}

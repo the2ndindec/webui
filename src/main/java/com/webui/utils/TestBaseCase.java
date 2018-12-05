4184940c9462614f5bf59aff9dbd164121ca1714
lijinglin <the2ndindec@163.com>
2018-12-05 14:53:25 +0800

    更新参数方法

--- src/main/java/com/webui/utils/TestBaseCase.java
+++ src/main/java/com/webui/utils/TestBaseCase.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
@@ -16,56 +17,61 @@ import io.qameta.allure.Step;
 
 public class TestBaseCase {
 
-	public static WebDriver driver;
-	public static String description;
-	public Log log = new Log(this.getClass().getSuperclass());
+    public static WebDriver driver;
+    public static String description;
+    public Log log = new Log(this.getClass().getSuperclass());
 
-	@BeforeSuite(description="初始化浏览器配置，加载访问地址")
-	@Parameters({ "driver"})
-	public void loadConfiguration(String driver) {
-		log.info("------------------开始执行测试---------------");
-		try {
-			TestBaseCase.driver = setDriver(driver);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		TestBaseCase.driver.manage().window().maximize();
-		TestBaseCase.driver.get("http://192.168.3.186:8080/ty/");
-		TestBaseCase.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	}
+    @BeforeSuite(description = "初始化浏览器配置，加载访问地址")
+    @Parameters({"driver","url"})
+    public void loadConfiguration(String driver,String url) {
+        log.info("------------------开始执行测试---------------");
+        try {
+            TestBaseCase.driver = setDriver(driver);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        TestBaseCase.driver.manage().window().maximize();
+        TestBaseCase.driver.get(url);
+        TestBaseCase.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
 
-	@AfterSuite
-	public void tearDown() {
-		TestBaseCase.driver.quit();
-		log.info("-------------结束测试，并关闭退出浏览器-------------");
-	}
+    @AfterSuite
+    public void tearDown() {
+        TestBaseCase.driver.quit();
+        log.info("-------------结束测试，并关闭退出浏览器-------------");
+    }
 
-	@Step(value="初始化浏览器driver")
-	private WebDriver setDriver(String browsername) {
-		switch (browsername) {
-		case "FirefoxDriver":
-			TestBaseCase.driver = new FirefoxDriver();
-			break;
-		case "ChormeDriver":
-			System.setProperty("webdriver.chrome.driver", "src/main/resources/drives/ChromeDriver 2.41/chromedriver.exe");
-			ChromeOptions options = new ChromeOptions();
-			// ----设置chrome加载flash插件
-			Map<String, Object> prefs =new HashMap<String, Object>();
-			prefs.put("profile.default_content_setting_values.plugins", 1);
-			prefs.put("profile.default_content_setting_values.plugins", 1);
-		    prefs.put("profile.content_settings.plugin_whitelist.adobe-flash-player", 1);
-		    prefs.put("profile.content_settings.exceptions.plugins.*,*.per_resource.adobe-flash-player", 1);
-		    prefs.put("PluginsAllowedForUrls", "https://arlo.netgear.com");
-		    options.setExperimentalOption("prefs", prefs);  
-		    // ----设置chrome加载flash插件结束
-			options.addArguments("disable-infobars");
-			
-			TestBaseCase.driver = new ChromeDriver(options);
-			break;
-		default:
-			TestBaseCase.driver = new FirefoxDriver();
-			break;
-		}
-		return driver;
-	}
+    @Step(value = "初始化浏览器driver")
+    private WebDriver setDriver(String browsername) {
+        switch (browsername) {
+            case "FirefoxDriver":
+                FirefoxProfile firefoxProfile = new FirefoxProfile();
+                firefoxProfile.setPreference("browser.download.folderList", 2); //设置成0 代表下载到浏览器默认下载路径；设置成2 则可以保存到指定目录。
+                firefoxProfile.setPreference("browser.download.manager.showWhenStarting", false); //设置成0 代表下载到浏览器默认下载路径；设置成2 则可以保存到指定目录。
+                firefoxProfile.setPreference("browser.download.dir", "D:\\dev\\IdeaProjects\\webui\\src\\test\\resources\\autoDownload");   //用于指定你所下载文件的目录。
+                firefoxProfile.setPreference("browser.helperApps.neverAsk.saveToDisk", "application/vnd.ms-excel"); //指定要下载页面的Content-type 值，“application/octet-stream”为文件的类型
+                TestBaseCase.driver = new FirefoxDriver(firefoxProfile);
+                break;
+            case "ChormeDriver":
+                System.setProperty("webdriver.chrome.driver", "src/main/resources/drives/ChromeDriver 2.41/chromedriver.exe");
+                ChromeOptions options = new ChromeOptions();
+                // ----设置chrome加载flash插件
+                Map<String, Object> prefs = new HashMap<String, Object>();
+                prefs.put("profile.default_content_setting_values.plugins", 1);
+                prefs.put("profile.default_content_setting_values.plugins", 1);
+                prefs.put("profile.content_settings.plugin_whitelist.adobe-flash-player", 1);
+                prefs.put("profile.content_settings.exceptions.plugins.*,*.per_resource.adobe-flash-player", 1);
+                prefs.put("PluginsAllowedForUrls", "https://arlo.netgear.com");
+                options.setExperimentalOption("prefs", prefs);
+                // ----设置chrome加载flash插件结束
+                options.addArguments("disable-infobars");
+
+                TestBaseCase.driver = new ChromeDriver(options);
+                break;
+            default:
+                TestBaseCase.driver = new FirefoxDriver();
+                break;
+        }
+        return driver;
+    }
 }
